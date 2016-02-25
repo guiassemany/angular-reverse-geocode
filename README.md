9d6f359672f1fb93dc46b3e424525b513a34d1ac
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-02-25 13:20:42 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Assemany AngularJS Reverse Geocode
+=======================
+This is a simple directive to handle reverse geocode using google maps.
+
+### How to Installation
+
+Bower: `bower install assemany-angular-reverse-geocode`
+You can also simply download the file assemany-angular-reverse-geocode.js and import it to your page.
+
+After this, you should add 'AssemanyAngularReverseGeocode' as a dependency for your project.
+```javascript
+angular.module('myApplication', ['AssemanyAngularReverseGeocode']);
+```
+
+### How to use
+```html
+<reverse-geocode lat="14.14787" lng="-32.15478"></reverse-geocode>
+```
---
+++ assemany-angular-reverse-geocode.js
@@ -0,0 +1,30 @@
+﻿/**
+ * Assemany AngularJS reverse geocoding directive
+ * @author Guilherme Assemany <guilhermeassemany@hotmail.com> (http://assemany.com)
+ * @version 1.0.0
+ */
+(function () {
+    angular.module('AssemanyAngularReverseGeocode', [])
+    .directive('reverseGeocode', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            link: function (scope, element, attrs) {
+                var geocoder = new google.maps.Geocoder();
+                var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
+                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            element.text(results[0].formatted_address);
+                        } else {
+                            element.text('Location not found');
+                        }
+                    } else {
+                        element.text('Geocoder failed due to: ' + status);
+                    }
+                });
+            },
+            replace: true
+        };
+    });
+})();
