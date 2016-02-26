4b9b1fa5930293fe47a9e60a7b7f1ef494eaab6d
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-02-26 21:10:21 +0000

    Created distribution version and put main file on src folder

--- demo/app.js
+++ demo/app.js
@@ -7,6 +7,7 @@
         ])
         .controller('MyCtrl',['reverseGeocode', '$scope', function(reverseGeocode, $scope){
           var vm = this;
+          console.log('a');
           vm.lat = "-12.914090";
           vm.lng = "-38.493835";
 
---
+++ src/angular-reverse-geocode.js
@@ -0,0 +1,51 @@
+﻿/**
+ * AngularJS reverse geocoding directive
+ * @author Guilherme Assemany <guilhermeassemany@hotmail.com> (http://assemany.com)
+ * @version 1.0.0
+ */
+(function () {
+    angular.module('AngularReverseGeocode', [])
+    .service('reverseGeocode', ['$http', function($http){
+
+      var vm = this;
+
+      vm.geocodePosition = geocodePosition;
+
+      var geocoder = new google.maps.Geocoder();
+
+      function geocodePosition(lat, lng, fn) {
+        var latlng = new google.maps.LatLng(lat, lng);
+        geocoder.geocode({
+          latLng: latlng
+        }, function(responses) {
+          if (responses && responses.length > 0) {
+             fn(responses[0].formatted_address);
+          } else {
+             alert('Error while trying to find the location');
+          }
+        });
+      }
+    }])
+    .directive('reverseGeocode', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            link: function (scope, element, attrs) {
+                var geocoder = new google.maps.Geocoder();
+                var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
+                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            element.text(results[0].formatted_address);
+                        } else {
+                            element.text('Location not found');
+                        }
+                    } else {
+                        element.text('Geocoder failed due to: ' + status);
+                    }
+                });
+            },
+            replace: true
+        };
+    });
+})();
