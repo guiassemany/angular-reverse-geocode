e2ac9ffe01a8cbc4d315c9c9c7957f8ef699fdeb
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-02-26 12:49:06 -0300

    Created a reverseGeocode Service and included a demo

--- angular-reverse-geocode.js
+++ angular-reverse-geocode.js
@@ -5,6 +5,27 @@
  */
 (function () {
     angular.module('AngularReverseGeocode', [])
+    .service('reverseGeocode', ['$http', function($http){
+
+      var vm = this;
+
+      vm.geocodePosition = geocodePosition;
+
+      var geocoder = new google.maps.Geocoder();
+
+      function geocodePosition(lat, lng, fn) {
+        var latlng = new google.maps.LatLng(lat, lng);
+        geocoder.geocode({
+          latLng: latlng
+        }, function(responses) {
+          if (responses && responses.length > 0) {
+             fn(responses[0].formatted_address);
+          } else {
+             alert('Error while trying to find the location');
+          }
+        });
+      }
+    }])
     .directive('reverseGeocode', function () {
         return {
             restrict: 'E',
---
+++ demo/app.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('myApp', [
+            'AngularReverseGeocode'
+        ])
+        .controller('MyCtrl',['reverseGeocode', '$scope', function(reverseGeocode, $scope){
+          var vm = this;
+          vm.lat = "-12.914090";
+          vm.lng = "-38.493835";
+
+          vm.formatted_address = "";
+
+          activate();
+
+          function activate(){
+            reverseGeocode.geocodePosition(vm.lat, vm.lng, function(address){
+               vm.formatted_address = address;
+               $scope.$apply();
+            });
+          }
+
+        }]);
+
+})();
